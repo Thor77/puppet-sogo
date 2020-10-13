c7a35fb7e4f928fd4ba63b2588c6339fe65b5ef9
Matthias Kneer <matze@roterruler.de>
2020-10-13 23:01:27 +0200

    Change name to path for envconfig resource

--- manifests/envconfig.pp
+++ manifests/envconfig.pp
@@ -4,7 +4,7 @@ class sogo::envconfig {
   if ! empty($sogo::envconfig) {
     file { 'envconfig':
       ensure       => 'present',
-      name         => $sogo::envconfig_path,
+      path         => $sogo::envconfig_path,
       content      => epp('sogo/sogo.envconfig.epp', { envconfig => $sogo::envconfig }),
       validate_cmd => '/bin/bash -n %',
       notify       => Service[$sogo::service_name],
